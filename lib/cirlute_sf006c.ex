e9be917d4500ff1614899355c60c53332dac8870
Cocoa <r.xu.1@research.gla.ac.uk>
2022-04-05 12:04:47 +0100

    updated angle_to_analog

--- lib/cirlute_sf006c.ex
+++ lib/cirlute_sf006c.ex
@@ -2,6 +2,8 @@ defmodule Cirlute.SF006C do
   @moduledoc """
   Elixir driver for SunFounder SF006C Servo.
   """
+  @min_angle 0
+  @max_angle 180
 
   alias __MODULE__, as: T
 
@@ -25,6 +27,7 @@ defmodule Cirlute.SF006C do
             min_pulse_width: 600,
             max_pulse_width: 2400,
             default_pulse_width: 1500
+            pulse_width: 1500
 
   @type pwm_opt() :: {term(), non_neg_integer(), pos_integer(), number()}
   @type servo_opts() :: [pwm_opt: pwm_opt()]
@@ -38,7 +41,7 @@ defmodule Cirlute.SF006C do
   """
   @spec new(non_neg_integer(), servo_opts()) :: {:ok, term()} | {:error, term()}
   def new(channel, opts) do
-    with [pwm_module, pwm_address, pwm_i2c_bus, pwm_freq] <- Tuple.to_list(opts[:pwm_opts] || {}),
+    with [pwm_module, pwm_address, pwm_i2c_bus, pwm_freq] <- Tuple.to_list(opts[:pwm_opts] || {:error, "invalid pwm_opts"}),
          {:ok, pwm_driver} <- Kernel.apply(pwm_module, :new, [pwm_address, pwm_i2c_bus, pwm_freq]) do
       {:ok,
        %T{
@@ -51,10 +54,10 @@ defmodule Cirlute.SF006C do
          min_pulse_width: opts[:min_pulse_width] || 600,
          max_pulse_width: opts[:max_pulse_width] || 2400,
          default_pulse_width: opts[:default_pulse_width] || 1500
+         pulse_width: 1500
        }}
     else
       {:error, reason} -> {:error, reason}
-      _ -> {:error, "invalid pwm_opts"}
     end
   end
 
@@ -64,8 +67,8 @@ defmodule Cirlute.SF006C do
   - **self**: Cirlute.SF006C instance
   - **angle**: requested angle
   """
-  @spec set_angle(term(), integer()) :: {:ok, term()} | {:error, term()}
-  def set_angle(self = %T{}, angle) when is_integer(angle) and angle >= 0 and angle <= 180 do
+  @spec set_angle(term(), number()) :: {:ok, term()} | {:error, term()}
+  def set_angle(self = %T{}, angle) when is_number(angle) and angle >= @min_angle and angle <= @max_angle do
     offset = angle_to_analog(self, angle) + self.offset
 
     with :ok <-
@@ -74,18 +77,18 @@ defmodule Cirlute.SF006C do
              self.pwm_channel,
              offset
            ]) do
-      {:ok, self}
+      {:ok, %T{self | pulse_width: offset}}
     else
       {:error, reason} -> {:error, reason}
     end
   end
 
-  def set_angle(self = %T{}, angle) when is_integer(angle) and angle > 180 do
-    set_angle(self, 180)
+  def set_angle(self = %T{}, angle) when is_integer(angle) and angle > @max_angle do
+    set_angle(self, @max_angle)
   end
 
-  def set_angle(self = %T{}, angle) when is_integer(angle) and angle < 0 do
-    set_angle(self, 0)
+  def set_angle(self = %T{}, angle) when is_integer(angle) and angle < @min_angle do
+    set_angle(self, @min_angle)
   end
 
   defp map_value(x, in_min, in_max, out_min, out_max) do
@@ -93,7 +96,10 @@ defmodule Cirlute.SF006C do
   end
 
   defp angle_to_analog(self = %T{}, angle) do
-    pulse_width = map_value(angle, 0, 180, self.min_pulse_width, self.max_pulse_width)
-    trunc(pulse_width / 1_000_000.0 * self.pwm_frequency * 4096)
+    pulse_width = map_value(angle, @min_angle, @max_angle, self.min_pulse_width, self.max_pulse_width)
+    min_pwm = Kernel.apply(self.pwm_module, :min_pwm, [])
+    max_pwm = Kernel.apply(self.pwm_module, :max_pwm, [])
+    pwm_levels = max_pwm - min_pwm + 1
+    trunc(pulse_width / 1_000_000.0 * self.pwm_frequency * pwm_levels)
   end
 end
